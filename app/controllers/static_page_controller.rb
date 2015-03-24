d8b1b4b17d0585ff23f9bd23571e3151b2264d55
jtsai783 <jim.h.tsai@gmail.com>
2015-03-24 16:04:41 -0400

    sorted skill points

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -4,7 +4,7 @@ class StaticPageController < ApplicationController
 		@active_skills = ActiveSkill.select("active_skills.id, active_skills.name, active_skills.skill_id, active_skills.points").where('points > 0').order('active_skills.name').to_json.html_safe
 		@active_skills.gsub("'", %q(\\\'))
 
-		@skills = Skill.select("skills.id, skills.name").to_json.html_safe
+		@skills = Skill.select("skills.id, skills.name").order("skills.name").to_json.html_safe
 		render :root
 	end
 end
